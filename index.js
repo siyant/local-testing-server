59948aeadd6ecedc86ac29b46fbe28b72adb84fe
Teo Si-Yan <dev@teosiyan.com>
2020-11-24 17:39:53 +0800

    Add multer and endpoint that receives & stores files

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+const fs = require("fs");
 const express = require("express");
+const multer = require("multer");
+
 const app = express();
 const port = 4000;
+const uploadsDir = "uploads";
+
+if (!fs.existsSync(uploadsDir)) fs.mkdirSync(uploadsDir);
+
+const storage = multer.diskStorage({
+  destination: uploadsDir,
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 
 app.get("/", (req, res) => res.json({ message: "Hello World!" }));
+
+app.post("/upload", upload.single("file"), function (req, res) {
+  console.log(`Received file: ${req.file.originalname}`);
+  res.json({ success: true });
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
