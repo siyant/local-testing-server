8ab4b6cdd864b0ba07f97e431257825113838f5a
Teo Si-Yan <dev@teosiyan.com>
2021-02-19 14:16:16 +0800

    Log request url, headers and body

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 const port = 4000;
 const uploadsDir = "uploads";
 
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 if (!fs.existsSync(uploadsDir)) fs.mkdirSync(uploadsDir);
 
 const storage = multer.diskStorage({
@@ -20,6 +23,17 @@ app.get("/", (req, res) => res.json({ message: "Hello World!" }));
 
 app.post("/upload", upload.single("file"), function (req, res) {
   console.log(`Received file: ${req.file.originalname}`);
+  res.json({ success: true });
+});
+
+app.all("*", function (req, res, next) {
+  console.log("\n\n\n\n\n----------------- received request -----------------");
+  console.log("req.method", req.method);
+  console.log("req.url", req.url);
+  console.log("\n\n--------- headers ---------");
+  console.log(req.headers);
+  console.log("\n\n--------- body ---------");
+  console.log(req.body);
   res.json({ success: true });
 });
 
